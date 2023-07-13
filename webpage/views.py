6da3556149487ba861797598c13e8aa8483251f1
JERMANYs <117138009+JERMANYs@users.noreply.github.com>
2023-07-13 11:20:28 +0700

    add statics and dir

--- webpage/views.py
+++ webpage/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse  
 
 # Create your views here.
+def home(request):
+    return HttpResponse("Hello, this is my first home page django app")
+
+def about(request):
+    return HttpResponse("About us")
+
+def contact(request):
+    return HttpResponse("Contact us")
