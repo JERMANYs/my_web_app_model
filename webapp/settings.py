eb979450132645d428db73b7f7c5ce7d55458115
JERMANYs <117138009+JERMANYs@users.noreply.github.com>
2023-08-24 11:17:42 +0700

    db json

--- webapp/settings.py
+++ webapp/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'webpage',
+    'django_dump_load_utf8',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,14 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE' : 'django.db.backends.postgresql_psycopg2' ,
+        'NAME' : 'press_db' ,
+        'USER' : 'postgres' ,
+        'PASSWORD' : '123456',
+        'HOST' : 'localhost' ,
+        'PORT' : '',
     }
 }
 
